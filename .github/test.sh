4a0072a06aa7e46416133166530eb669756db25d
matsalinas <matsalinas@uchile.cl>
2021-03-09 20:53:26 +0000

    Update tests Koa

--- .github/test.sh
+++ .github/test.sh
@@ -2,14 +2,14 @@
 
 pip install -e /openedx/requirements/eolcontainer
 
-cd /openedx/requirements/eolcontainer/eolcontainer
+cd /openedx/requirements/eolcontainer
 cp /openedx/edx-platform/setup.cfg .
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/eolcontainer/eolcontainer
+cd /openedx/requirements/eolcontainer
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest eolcontainer/tests.py
 
 rm -rf test_root
