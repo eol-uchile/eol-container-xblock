270305d91fe1c43afdf4f2d129fb4b77c71a896b
Lozamded <dhiguerafernandez@gmail.com>
2024-09-30 13:00:02 -0300

    ADD: Limited mathjax render
    ADD: Remove unused logs
    
    ADD: rename mathjax

--- eolcontainer/static/js/src/eolcontainer.js
+++ eolcontainer/static/js/src/eolcontainer.js
@@ -1,66 +1,82 @@
 /*
-       _=,_
-    o_/6 /#\
-    \__ |##/
-     ='|--\
-       /   #'-.
-       \#|_   _'-. /
-        |/ \_( # |" 
-       C/ ,--___/
+	   _=,_
+	o_/6 /#\
+	\__ |##/
+	 ='|--\
+	   /   #'-.
+	   \#|_   _'-. /
+		|/ \_( # |" 
+	   C/ ,--___/
 */
 
 function EolContainerXBlock(runtime, element, settings) {
 
-    $(function ($) {
-		if(settings.type == "Respuesta") {
-			$('#' + settings.type + settings.location +' .expmid').toggle();
+	$(function ($) {
+		if (settings.type == "Respuesta") {
+			$('#' + settings.type + settings.location + ' .expmid').toggle();
 		}
 
-		MathJax.Hub.Queue(["Typeset", MathJax.Hub]); //Reconstruir latex
-    });
+		var containerid = "container_" + settings.location;
+		renderMathForSpecificElements(containerid);
+	});
 }
 
-function toggleContainer(type, id) {
-	var elemento = $('#' + type + id +' .expbot');
+function renderMathForSpecificElements(id) {
+	//console.log("Render Mathjax in " + id);
+	if (typeof MathJax !== "undefined") {
+		var $container = $('#' + id);
+		if ($container.length) {
+			$container.find('.exptop, .expmid, .expbot').each(function (index, contaelem) {
+				MathJax.Hub.Queue(["Typeset", MathJax.Hub, contaelem]);
+			});
+		}
+	} else {
+		console.warn("MathJax no está cargado.");
+	}
+}
 
-	if (elemento.hasClass("toggle")) 	{
+
+function toggleContainer(type, id) {
+	var elemento = $('#' + type + id + ' .expbot');
+
+	if (elemento.hasClass("toggle")) {
 		elemento.removeClass("toggle");
 	}
 	else {
 		elemento.addClass("toggle");
 	}
 
-	$("body, html").stop(true,true);
-	$("body, html").animate({ 
-        scrollTop: $('#' + type + id).offset().top 
-    }, 1000);
-	$('#' + type + id +' .expmid').animate({
+	$("body, html").stop(true, true);
+	$("body, html").animate({
+		scrollTop: $('#' + type + id).offset().top
+	}, 1000);
+	$('#' + type + id + ' .expmid').animate({
 		height: "toggle"
-	},1000);
+	}, 1000);
 }
 
 
 function toggleContainerMedia(type, id) {
-	var elemento = $('#' + type + id +' .expbot');
-	var elemento2 = $('#' + type + id );
-	var elemento3 = $('#' + type + id +' .expmid');
+	var elemento = $('#' + type + id + ' .expbot');
+	var elemento2 = $('#' + type + id);
+	var elemento3 = $('#' + type + id + ' .expmid');
 
-	if (elemento.hasClass("toggle")) 	{
+	if (elemento.hasClass("toggle")) {
 		elemento.removeClass("toggle");
 	}
 	else {
 		elemento.addClass("toggle");
 	}
 
-	$("body, html").stop(true,true);
-	$("body, html").animate({ 
-        scrollTop: elemento2.offset().top 
-    }, 1000);
+	$("body, html").stop(true, true);
+	$("body, html").animate({
+		scrollTop: elemento2.offset().top
+	}, 1000);
 	elemento3.animate({
 		height: "toggle"
-	},1000, function(){
+	}, 1000, function () {
 		console.log("fin");
-		if (elemento.hasClass("cerrado")) 	{
+		if (elemento.hasClass("cerrado")) {
 			elemento.removeClass("cerrado");
 		}
 		else {
