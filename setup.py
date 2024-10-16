bae71fd95568335a7f632f04eb3eb0ccde7c93d9
Lozamded <dhiguerafernandez@gmail.com>
2024-10-16 17:02:32 -0300

    FIX: Sistematizacion header size

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def package_data(pkg, roots):
 
 setup(
     name='eolcontainer-xblock',
-    version='0.1',
+    version='1.0.1',
     description='XBlock con capsulas Suma y Sigue de CMMEduFormacion',
     license='AGPL v3',
     packages=[
