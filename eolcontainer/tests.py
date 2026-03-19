f1396472c8bfa80f104bf0876f0aa2b593fac300
Vicente Astorga <vicente.astorga.dev@gmail.com>
2026-03-19 17:33:34 -0300

    test error in test.py
    Removed unused imports and commented-out code from tests.py.

--- eolcontainer/tests.py
+++ eolcontainer/tests.py
@@ -1,48 +1,4 @@
-# -*- coding: utf-8 -*-
 
-from mock import patch, Mock
-
-
-from django.test import TestCase, Client
-
-from common.djangoapps.util.testing import UrlResetMixin
-from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
-
-from xmodule.modulestore.tests.factories import CourseFactory
-from common.djangoapps.student.tests.factories import UserFactory, CourseEnrollmentFactory
-from xblock.field_data import DictFieldData
-from common.djangoapps.student.roles import CourseStaffRole
-
-from .eolcontainer import EolContainerXBlock
-
-from six import text_type
-
-import logging
-logger = logging.getLogger(__name__)
-
-XBLOCK_RUNTIME_USER_ID = 99
-
-class TestEolContainerXBlock(UrlResetMixin, ModuleStoreTestCase):
-
-    def make_an_xblock(self, **kw):
-        """
-        Helper method that creates a EolContainer XBlock
-        """
-        course = self.course
-        runtime = Mock(
-            course_id=course.id,
-            service=Mock(
-                return_value=Mock(_catalog={}),
-            ),
-            user_id=XBLOCK_RUNTIME_USER_ID,
-        )
-        scope_ids = Mock()
-        field_data = DictFieldData(kw)
-        xblock = EolContainerXBlock(runtime, field_data, scope_ids)
-        xblock.xmodule_runtime = runtime
-        xblock.location = course.id  # Example of location
-        xblock._edited_by = XBLOCK_RUNTIME_USER_ID
-        return xblock
 
     def setUp(self):
 
