f8b4e73914a3bbf92ce3197f7a5d2fd33ecc635e
Lozamded <dhiguerafernandez@gmail.com>
2026-07-30 10:49:40 -0400

    chore: update version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def package_data(pkg, roots):
 
 setup(
     name='eolcontainer-xblock',
-    version='1.2.0',
+    version='1.2.1',
     description='XBlock con capsulas Suma y Sigue de CMMEduFormacion',
     license='AGPL v3',
     packages=[
